4e0e356ab5dbcc314c09a9c5b865f49a877d3ada
Jordan Bach <jordanbach@gmail.com>
2015-08-07 13:17:57 -0500

    add event handler registration

--- lib/chasqui/subscriber.rb
+++ lib/chasqui/subscriber.rb
@@ -1,6 +1,17 @@
 module Chasqui
   class Subscriber
 
+    def on(event_name, &block)
+      @handlers ||= {}
+      @handlers[event_name] = block
+    end
+
+    def handlers_for(event_name)
+      @handlers ||= {}
+      keys = @handlers.keys.grep(/#{event_name.gsub('*', '.*')}/)
+      keys.map { |k| @handlers[k] }
+    end
+
     def evaluate(&block)
       @self_before_instance_eval = eval "self", block.binding
       instance_eval &block
---
+++ spec/lib/chasqui/subscriber_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe Chasqui::Subscriber do
+
+  describe '#on' do
+    let(:subscriber) { Chasqui::Subscriber.new }
+
+    it 'registers the event handlers' do
+      subscriber.on('foo') { |foo| foo }
+      subscriber.on('zig.zag') { |*args| args }
+
+      handler = subscriber.handlers_for('foo').first
+      expect(handler.call('bar')).to eq('bar')
+
+      handler = subscriber.handlers_for('zig.zag').first
+      expect(handler.call(1, 2, 3, 4)).to eq([1, 2, 3, 4])
+    end
+  end
+
+  describe '#handlers_for' do
+    let(:subscriber) { Chasqui::Subscriber.new }
+
+    it 'always returns an array' do
+      expect(subscriber.handlers_for('unknown')).to eq([])
+    end
+
+    it 'matches single event' do
+      p = Proc.new { }
+      subscriber.on('foo', &p)
+      expect(subscriber.handlers_for('foo')).to eq([p])
+    end
+
+    it 'matches wildcard postfixes' do
+      p = 3.times.map { Proc.new { } }
+      subscriber.on('foo', &p[0])
+      subscriber.on('bar', &p[1])
+      subscriber.on('foo.bar', &p[2])
+      expect(subscriber.handlers_for('foo*')).to eq([p[0], p[2]])
+    end
+
+    it 'matches wildcard prefixes' do
+      p = 3.times.map { Proc.new { } }
+      subscriber.on('foo', &p[0])
+      subscriber.on('bar', &p[1])
+      subscriber.on('foo.bar', &p[2])
+      expect(subscriber.handlers_for('*bar')).to eq([p[1], p[2]])
+    end
+
+    it 'matches all events' do
+      p = 3.times.map { Proc.new { } }
+      subscriber.on('foo', &p[0])
+      subscriber.on('bar', &p[1])
+      subscriber.on('foo.bar', &p[2])
+      expect(subscriber.handlers_for('*')).to eq([p[0], p[1], p[2]])
+    end
+  end
+
+end
