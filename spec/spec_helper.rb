c9d2660688ccc9a5f37d5ee6f9cd7afc75cb93be
Jordan Bach <jordanbach@gmail.com>
2015-08-17 22:38:36 -0500

    need to be more specific

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ end
 # Newer versions of sidekiq only support newer versions of ruby
 # https://github.com/mperham/sidekiq/blob/master/Changes.md#322
 def sidekiq_supported_ruby_version?
-  Gem::Version.new(RUBY_VERSION) > Gem::Version.new('1.9')
+  Gem::Version.new(RUBY_VERSION) > Gem::Version.new('1.9.3')
 end
 
 class FakeSubscriber < Chasqui::Subscriber
